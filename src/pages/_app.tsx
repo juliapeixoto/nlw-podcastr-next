ac23c8f1d03db3854cb509a9258175142e3f8ff4
juliap <juliap@ciandt.com>
2021-05-04 13:34:57 -0300

    play and pause button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,14 +10,33 @@ import { useState } from "react";
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setisPlaying] = useState(false);
 
   function play(episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setisPlaying(true);
+  }
+
+  function togglePlay() {
+    setisPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setisPlaying(state);
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
       <div className={styles.wrapper}>
         <main>
           <Header />
