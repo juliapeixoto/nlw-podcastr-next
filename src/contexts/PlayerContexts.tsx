9ab93c866c2c79355c8629ff766a939b98c52bd6
juliap <juliap@ciandt.com>
2021-05-07 15:56:21 -0300

    fixing files organization

--- src/contexts/PlayerContexts.tsx
+++ src/contexts/PlayerContexts.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState, ReactNode } from "react";
 
 type Episode = {
   title: string;
@@ -13,8 +13,57 @@ type PlayerContextData = {
   currentEpisodeIndex: number;
   isPlaying: boolean;
   play: (episode: Episode) => void;
+  playList: (list: Episode[], index: number) => void;
   setPlayingState: (state: boolean) => void;
   togglePlay: () => void;
 };
 
 export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+  children: ReactNode;
+};
+
+export function PlayerContextProvider({
+  children,
+}: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setisPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setisPlaying(true);
+  }
+
+  function playList(list: Episode[], index: number) {
+    setEpisodeList(list);
+    setCurrentEpisodeIndex(index);
+    setisPlaying(true);
+  }
+
+  function togglePlay() {
+    setisPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setisPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        playList,
+        isPlaying,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
